856d5b27adbb638658969be9d979bb763f163a84
PhungTheAnh <phungtheanh2001@gmail.com>
2022-08-05 18:32:59 +0700

    Calculator

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const display = document.getElementById('display');
+const buttons = Array.from(document.querySelectorAll('.button'));
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'AC':
+                display.innerText = '';
+                break;
+            case 'Delete':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = '';
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
